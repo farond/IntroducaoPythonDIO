3c1a817c48593caeab3a0b21e2b3b2446521248e
farond <fabio_lino@outlook.com>
2020-09-18 14:47:06 -0300

    Aula 11 completa

--- aula11.py
+++ aula11.py
@@ -1,7 +1,48 @@
 
-lista = [1,10]
+# lista = [1,10]
+# try:
+#     divisao = 10 / 0
+#     numero = lista[1]
+# except ZeroDivisionError:
+#     print('Nao é possível realizar uma divisão por 0')
+
+# lista = [1,10]
+# try:
+#     divisao = 10 / 1
+#     numero = lista[3]
+#
+# except ZeroDivisionError:
+#     print('Nao é possível realizar uma divisão por 0')
+# except IndexError:
+#     # print('Erro ao acessar um indice inválido da lista')
+
+lista = [1, 10]
+arquivo = open ('test.text', 'r')
 try:
-    divisao = 10 / 0
+    texto = arquivo.read()
+    divisao = 10 / 1
+    #divisao = 10 / 0
     numero = lista[1]
+    #x = a
+
+#funcionamento em arvore
 except ZeroDivisionError:
     print('Nao é possível realizar uma divisão por 0')
+except ArithmeticError:
+    print('Houve um erro ao realizar uma operação Aritmética')
+except IndexError:
+    print('Erro ao acessar um indice inválido da lista')
+except BaseException as ex: #Pai de todas as except
+    print('Erro desconhecido. Erro: {}'.format(ex))
+
+else:
+    print('Executa quando não ocorre exceção')
+
+finally: #dando erro ou não, irá executar o que está dentro dele
+    print('Sempre executa')
+    print('Arquivo fechado')
+    arquivo.close()
+
+
+
+
